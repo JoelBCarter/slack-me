24f393fdcb63164a2408433b4d1ea63931fb456c
Joel <joelbcarter@gmail.com>
2017-06-18 00:04:11 -0500

    Passing unit tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,24 +1,42 @@
 const { expect } = require('chai'),
     path = require('path'),
+    proxyquire = require('proxyquire'),
     sinon = require('sinon'),
-    sut = require(path.join('..', 'index.js'));
+    sut = path.join(__dirname, '..', 'index.js');
+
 describe('slack-it-to-me', function () {
     let stub;
     beforeEach(() => {
-        stub = sinon.stub(require('node-slack'), 'send');
-    });
-    afterEach(() => {
-        stub.restore();
+        stub = sinon.stub();
     });
     describe('should post', function () {
-        it('to the URL specified by the ENV VAR', function () {
-            throw new Error('TODO: Write test.');
+        it('to the URL specified by the ENV VAR', () => {
+            proxyquire(sut, { 'node-slack': stub });
+            expect(stub.args.toString()).to.be.eq(process.env.SLACK_WEBHOOK_URL);
         });
-        it('a 👍 when called with no args', function () {
-            throw new Error('TODO: Write test.');
+        it('a 👍 when called with no args', () => {
+            proxyquire(sut, {
+                'node-slack': function (url) {
+                    return { 'send': stub };
+                }
+            });
+            expect(stub.args[0][0]).to.be.eql({ text: '👍' });
         });
-        it('the piped text when cmd line output is piped to it', function () {
-            throw new Error('TODO: Write test.');
+        it('the piped text when cmd line output is piped to it', () => {
+            const pipedText = [
+                'Testing',
+                'multiline',
+                'piped text',
+            ];
+            const stdin = require('mock-stdin').stdin();
+            proxyquire(sut, {
+                'node-slack': function (url) {
+                    return { 'send': stub };
+                }
+            });
+            stdin.send(pipedText);
+            stdin.send(null); // trigger EOF & dispatch an 'end' event.
+            expect(stub.args[0][0].text).to.be.eql(pipedText.join('\n'));
         });
     });
 });
