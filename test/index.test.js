c14a680843a945b0972cbafed53eb6528ddfee7f
Joel <joelbcarter@gmail.com>
2017-06-17 22:11:45 -0500

    Unit test scaffolding

---
+++ test/index.test.js
@@ -0,0 +1,17 @@
+const { expect } = require('chai'),
+    path = require('path'),
+    sinon = require('sinon'),
+    sut = require(path.join('..', 'index.js'));
+describe('slack-it-to-me', function () {
+    describe('should post', function () {
+        it('to the URL specified by the ENV VAR', function () {
+            throw new Error('TODO: Write test.');
+        });
+        it('a 👍 when called with no args', function () {
+            throw new Error('TODO: Write test.');
+        });
+        it('the piped text when cmd line output is piped to it', function () {
+            throw new Error('TODO: Write test.');
+        });
+    });
+});
